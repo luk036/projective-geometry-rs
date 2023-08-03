07928bd8c8a8cff8d233a45cc9917458a239b7b4
Wai-Shing Luk <luk036@gmail.com>
2023-08-03 02:29:32 +0000

    fix the build problem

--- src/pg_plane.rs
+++ src/pg_plane.rs
@@ -1,6 +1,6 @@
 /// The code is defining a trait called `ProjectivePlanePrimitive` for a projection plane. This trait
 /// has two associated functions:
-pub trait ProjectivePlanePrimitive<Dual> : Eq {
+pub trait ProjectivePlanePrimitive<Dual>: Eq {
     fn incident(&self, dual: &Dual) -> bool;
     fn circ(&self, other: &Self) -> Dual;
 }
@@ -10,9 +10,6 @@ where
     Primal: ProjectivePlanePrimitive<Dual>,
     Dual: ProjectivePlanePrimitive<Primal>,
 {
-    if p != p || q != q {
-        return false;
-    }
     let l = p.circ(q);
     let m = q.circ(p);
     if l != m {
@@ -23,22 +20,21 @@ where
 
 /// The `coincident` function checks if three points `p`, `q`, and `r` are collinear in a projective
 /// plane.
-/// 
+///
 /// Arguments:
-/// 
+///
 /// * `p`: A point in the projective plane.
 /// * `q`: The parameter `q` is of type `&Primal`, which means it is a reference to an object of type
 /// `Primal`.
 /// * `r`: The parameter `r` represents a point in the projective plane.
-/// 
+///
 /// Returns:
-/// 
+///
 /// The function `coincident` returns a boolean value.
 pub fn coincident<Primal, Dual>(p: &Primal, q: &Primal, r: &Primal) -> bool
 where
     Primal: ProjectivePlanePrimitive<Dual>,
     Dual: ProjectivePlanePrimitive<Primal>,
 {
-    p.circ(q).incident(r)    
+    p.circ(q).incident(r)
 }
-
